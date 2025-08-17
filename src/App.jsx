6ed504b3efa9303c8b94892324ca4d03c3722efa
IsuruSampathRanasinghe <isuruep1@gmail.com>
2025-08-17 22:38:28 +0530

    tailwind css ✏️

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,17 @@ function App() {
 
   return (
     <>
-      <ProductCard name="Apple iPad" price="$499" image="https://cdn.macstories.net/13359d69-6cbb-4ade-8a47-dc272b9a8849-1632256898935.jpeg"/>
-      <ProductCard name="Mac Book Pro" price="$1299" image="https://media.wired.com/photos/643d7e61cdba28f045ac3f59/4:3/w_2136,h_1602,c_limit/macbook_sec_GettyImages-1368668740.jpg"/>
+
+      <div className='h-[700px] w-[700px] border-[5px] flex justify-center items-center'>
+        <div className='w-[600px] h-[600px] bg-yellow-300 flex flex-row justify-center items-center'>
+          <div className='w-[100px] h-[100px] bg-red-500'></div>
+          <div className='w-[100px] h-[100px] absolute top-[10px] right-[10px] bg-blue-500'></div>
+          <div className='w-[100px] h-[100px] bg-green-500'></div>
+          <div className='w-[100px] h-[100px] fixed right-[0px] bottom-[0px] bg-pink-500'></div>
+          <div className='w-[100px] h-[100px] bg-gray-500'></div>
+        </div>
+      </div>
+
     </>
   )
 }
