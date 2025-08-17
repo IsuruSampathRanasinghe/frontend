329f41e705d11607efdfc1cb03b0e29197ab7271
IsuruSampathRanasinghe <isuruep1@gmail.com>
2025-08-17 15:41:21 +0530

    frontend start ❤️

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ProductCard from './compnents/productCard'
+
+function App() {
+
+  return (
+    <>
+      <ProductCard name="Apple iPad" price="$499" image="https://cdn.macstories.net/13359d69-6cbb-4ade-8a47-dc272b9a8849-1632256898935.jpeg"/>
+      <ProductCard name="Mac Book Pro" price="$1299" image="https://media.wired.com/photos/643d7e61cdba28f045ac3f59/4:3/w_2136,h_1602,c_limit/macbook_sec_GettyImages-1368668740.jpg"/>
+    </>
+  )
+}
+
+export default App
---
+++ src/compnents/productCard.jsx
@@ -0,0 +1,14 @@
+import "./productCard.css"
+export default function ProductCard(props){
+    console.log(props.name);
+    return(
+        <div className="productCard">
+            <h1>{props.name}</h1>
+            <p>{props.price}</p>
+            <img 
+                className="productImage" 
+                src={props.image}/>
+            <button>Add to cart</button>
+        </div>
+    )
+}
